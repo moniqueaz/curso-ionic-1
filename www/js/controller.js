f0acdf04103bf0e8db8bdbdacf1f787d24cffe8c
Monique Azevedo <moniqueaz.si@gmail.com>
2017-09-06 16:11:43 -0300

    Inclusao e configuracao de plugin para tirar foto para o perfil | pasta lib está sendo ignorada pelo git

--- www/js/controller.js
+++ www/js/controller.js
@@ -93,13 +93,30 @@ angular.module('starter')
 });
 
 angular.module('starter')
-.controller('PerfilController', function($rootScope, $scope){
+.controller('PerfilController', function($rootScope, $scope, $cordovaCamera){
 
     // refatorar e colocar em uma function
     $scope.estaEditando = false;
     $scope.textoBotao = 'Editar';
 
     $scope.usuarioLogado = $rootScope.usuario;
+
+    $scope.tirarFoto = function(){
+        var opcoes = {
+            correctOrientation : true, // orientacao da foto de acordo com a camera
+            quality : 70, // qualidade %
+            saveToPhotoAlbum: false, // salvar no album
+            cameraDirection: 1, // frontal
+            targetWidth: 100, // testar
+            targetHeight: 100 // testar
+        }
+
+        $cordovaCamera.getPicture(opcoes).then(function(foto){
+            $scope.caminhoFoto = foto;
+        }, function(erro){
+
+        });
+    }
 
     $scope.acaoBotao = function(){
         if($scope.estaEditando){
@@ -109,6 +126,6 @@ angular.module('starter')
             $scope.estaEditando = true;
             $scope.textoBotao = 'Salvar';
         }
-    };
+    }
 });
 
