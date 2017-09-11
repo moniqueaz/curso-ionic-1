247c99a8c0934810da4fdcf386f8f2bc338baed5
Monique Azevedo <moniqueaz.si@gmail.com>
2017-09-11 12:40:44 -0300

    Salvando dados do usuario com webSQL prevendo erro da aplicacao

--- www/js/controllers/finalizarpedido.controller.js
+++ www/js/controllers/finalizarpedido.controller.js
@@ -1,6 +1,6 @@
 angular.module('starter')
 .controller('FinalizarPedidoController', function($stateParams, $scope, $ionicPopup, $state, 
-    CarroService, $ionicHistory, ionicDatePicker){
+    CarroService, $ionicHistory, ionicDatePicker, DatabaseValues){
 
     $scope.carroFinalizado = angular.fromJson($stateParams.carro);
 
@@ -18,7 +18,7 @@ angular.module('starter')
             monthsList: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"]
         }
         ionicDatePicker.openDatePicker(configuracoes)
-    }
+    } // fim do scope
 
     $scope.finalzarPedido = function(){
 
@@ -33,21 +33,40 @@ angular.module('starter')
         }
 
         CarroService.salvarPedido(pedidoFinalizado).then(function(dados){
+
+            $scope.salvarDadosNoBancoDeDados('true');
+
+            $ionicHistory.nextViewOptions({
+                disableBack : true
+            });
+
             $ionicPopup.alert({
                 title : 'Parabens',
                 template : 'Você acaba de comprar um carro'
             }).then(function(){
-                $ionicHistory.nextViewOptions({
-                    disableBack : true
-                });
                 $state.go('app.listagem');
             });
         }, function(erro){
+
+            $scope.salvarDadosNoBancoDeDados('false');
+           
             $ionicPopup.alert({
                 title : 'Deu erro',
-                template : 'Campos obrigatorios'
+                template : 'Servidor com problemas'
+            }).then(function(){
+                $state.go('app.listagem');
+            });
+        });
+
+        $scope.salvarDadosNoBancoDeDados = function(confirmado){
+            
+              DatabaseValues.setup();
+              DatabaseValues.bancoDeDados.transaction(function(transacao){
+                transacao.executeSql('INSERT INTO agendamentos(nome, endereco, email, dataAgendamento, modelo, preco, confirmado) VALUES (?,?,?,?,?,?,?)', [$scope.pedido.nome, $scope.pedido.endereco, $scope.pedido.email, $scope.dataSelecionada, $scope.carroFinalizado.nome, $scope.carroFinalizado.preco, confirmado])
             });
         }
-        );
-    }
-});
+
+
+
+    } // fim do scope
+}); // fim controller
